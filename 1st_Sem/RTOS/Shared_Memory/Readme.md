d5deaab747d7eb8339c147c43465c14762953c12
PrakrathiShriyan <147907394+PrakrathiShriyan@users.noreply.github.com>
2023-10-23 08:44:12 +0000

    code

---
+++ 1st_Sem/RTOS/Shared_Memory/Readme.md
@@ -0,0 +1,1 @@
+This folder contains Shared Memory problems
---
+++ 1st_Sem/RTOS/Shared_Memory/prog1.c
@@ -0,0 +1,44 @@
+/* 1. To demonstrate the usage of the various shared memory system
+calls- WAP which creates a shared memory segment of length 100 bytes
+and writes 'A' to 'Z' to it.
+Next, let the same program access the segment area and display its
+contents to the screen (all 100 bytes).Let the program next convert
+the case of the alphabets in the segment to lowercase. Finally let
+the program access the same area and display the segment contents
+once again to the screen. Observe the output.*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/shm.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <unistd.h>
+
+int main()
+{
+    int seg_id;
+    char *shm;
+    char *shm1;
+    char ch='a';
+    const int len=100;
+    int low=32;
+    int id1, id2, data=0;
+
+    id1=fork();
+    if(id>0)
+    {
+        printf("I am first parent\n");
+        seg_id=shmget(IPC_PRIVATE, len, S_IRUSR | S_IWUSR);
+        sm=(char *)shmat(seg_id, NULL, 0);
+        sm1=(char *)shmat(seg_id, NULL, 0);
+
+        for(int i=0; i<=10; i++)
+        {
+            *(sm+i)=data;
+            data++;
+        }
+        
+        for(int i; i<=36)
+    }
+}
