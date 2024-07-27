f1273cbb028e208e35abb673917152f92fe275ff
PrakrathiShriyan <147907394+PrakrathiShriyan@users.noreply.github.com>
2024-07-27 17:02:37 +0000

    added python

---
+++ Python/hello.py
@@ -0,0 +1,15 @@
+#docs.python.org
+# 1st program
+print('Hii')
+#/ escape character
+print("\"friend\"")
+name = input("What's your name? ")
+print("Hello",name)
+# print(*objects, sep=' ', end='\n', file=sys.stdout, flush=False)
+
+"""
+for(i=0; i<5, i++)
+{
+    print("Hweloo")
+}
+"""
